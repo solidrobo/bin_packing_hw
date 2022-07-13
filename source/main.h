10d7248e3f8a70d2a421abada48dcc98bd4c5025
Kalle-Gustav Kruus <kalle.gustav.kruus@gmail.com>
2022-07-13 22:55:11 +0300

    packing algorithm now works

--- source/main.h
+++ source/main.h
@@ -4,10 +4,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct {
-   char * str;
-   size_t len;
-} String_t;
+#define OUTPUT_ROWS 10
+#define OUTPUT_COLS 10
+
+typedef struct{
+   char output[OUTPUT_COLS][OUTPUT_ROWS];
+   int words;
+   int fill_level;
+   int empty_cells;
+   int first_empty_cell;
+} packing_result_t;
 
 int read_data(FILE * file, char buffer[], size_t buffer_len);
 int str_comp_ascending(const void* a, const void *b);
@@ -18,4 +24,8 @@ void update_score(char * permutation[], size_t array_len);
 void swap_index(char * array[],int index_a, int index_b);
 
 void permute(char * input[], int current_index, int array_len);
+
+void print_output(char buffer[OUTPUT_COLS][OUTPUT_ROWS]);
+
+int count_empy_cells(char buffer[OUTPUT_COLS][OUTPUT_ROWS]);
 #endif
